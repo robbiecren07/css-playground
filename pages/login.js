4492d93d9d089c002dc5d8c6c1014075105e339f
Robbie Crenshaw <robbiecdesigns@gmail.com>
2022-05-18 20:49:48 -0400

    commit

--- pages/login.js
+++ pages/login.js
@@ -7,30 +7,30 @@ import { Button, useColorModeValue } from '@chakra-ui/react'
 import styles from '../styles/Dashboard.module.scss'
 
 export default function Login() {
-
   const auth = useAuth()
   const color = useColorModeValue('#080808', '#fff')
 
   const title = 'Log Into My Account | CSS Playground'
-  const description = 'Log in or sign up for an account with CSS Playground. Get notified when new features are released.'
+  const description =
+    'Log in or sign up for an account with CSS Playground. Get notified when new features are released.'
   const url = 'https://www.cssplayground.dev/login'
 
   return (
     <>
-      <OpenGraph
-        title={title}
-        description={description}
-        url={url}
-      />
+      <OpenGraph title={title} description={description} url={url} />
       <main className={styles.main}>
         <div className={styles.auth_wrap}>
-
           <div className={styles.auth_form}>
             <h1>Hey!</h1>
-            <p>Use your email or another service to continue with CSS Playground.</p>
+            <p>
+              Use your email or another service to continue with CSS Playground.
+            </p>
 
             <div className={styles.btn}>
-              <Button onClick={(e) => auth.signinWithGitHub('/dashboard')} variant="outline-blue">
+              <Button
+                onClick={(e) => auth.signinWithGitHub('/dashboard')}
+                variant="outline-blue"
+              >
                 {/* <FontAwesomeIcon icon={faGithub} size="2x" /> */}
                 <GitHubIcon w="30px" h="30px" mr="16px" fill={color} />
                 Continue with GitHub
@@ -38,14 +38,15 @@ export default function Login() {
             </div>
 
             <div className={styles.btn}>
-              <Button onClick={(e) => auth.signinWithGoogle('/dashboard')} variant="outline-blue">
+              <Button
+                onClick={(e) => auth.signinWithGoogle('/dashboard')}
+                variant="outline-blue"
+              >
                 <GoogleIcon w="28px" h="28px" mr="16px" color={color} />
                 Continue with Google
               </Button>
             </div>
-
           </div>
-
         </div>
       </main>
     </>
--- styles/theme.js
+++ styles/theme.js
@@ -12,7 +12,7 @@ const theme = extendTheme({
   sizes: {
     container: {
       '2xl': '1560px',
-    }
+    },
   },
   fonts: {
     heading: 'Raleway, sans-serif',
@@ -44,7 +44,7 @@ const theme = extendTheme({
       600: '#074DC5',
       700: '#053A94',
       800: '#042662',
-      900: '#021331'
+      900: '#021331',
     },
     purple: {
       50: '#EFE7FD',
@@ -56,7 +56,7 @@ const theme = extendTheme({
       600: '#4F0DBF',
       700: '#3B0990',
       800: '#270660',
-      900: '#140330'
+      900: '#140330',
     },
     pink: {
       50: '#FCE8F2',
@@ -68,8 +68,8 @@ const theme = extendTheme({
       600: '#B51764',
       700: '#87124B',
       800: '#5A0C32',
-      900: '#2D0619'
-    }
+      900: '#2D0619',
+    },
   },
   styles: {
     global: (props) => ({
@@ -83,14 +83,13 @@ const theme = extendTheme({
         letterSpacing: '0.02rem',
       },
       a: {
-        color: 'gray.900',
-        fontWeight: '500',
+        color: mode('#080808', '#fff')(props),
+        fontWeight: mode('500', '400')(props),
         textDecoration: 'none',
         transition: 'all 0.3s ease-in-out',
         _hover: {
-          color: 'blue.400',
           textDecoration: 'none',
-        }
+        },
       },
       h1: {
         fontWeight: 'bold',
@@ -108,8 +107,8 @@ const theme = extendTheme({
       label: {
         fontWeight: mode('500', '400')(props),
       },
-    }
-  )},
+    }),
+  },
   components: {
     Link: {
       variants: {
@@ -118,7 +117,7 @@ const theme = extendTheme({
           _hover: {
             textDecoration: 'none',
             outline: 'none',
-          }
+          },
         }),
       },
     },
@@ -129,7 +128,7 @@ const theme = extendTheme({
         transition: 'all 0.3s ease-out',
       },
       sizes: {
-        'xl': {
+        xl: {
           w: '100%',
           h: '48px',
           px: '16px',
@@ -162,7 +161,7 @@ const theme = extendTheme({
           _active: {
             color: 'blue.400',
             bg: '#e5e7eb',
-          }
+          },
         }),
         'menu-button': (props) => ({
           height: '40px',
@@ -176,7 +175,7 @@ const theme = extendTheme({
           },
           _active: {
             bgGradient: 'linear-gradient(to bottom right, #7F3BF2, #357DF8)',
-          }
+          },
         }),
         // solid: (props) => ({
         //   bgGradient: 'linear-gradient(to bottom right, #7F3BF2, #357DF8)',
@@ -208,7 +207,7 @@ const theme = extendTheme({
           },
           _active: {
             bgGradient: 'linear-gradient(to bottom right, #7F3BF2, #357DF8)',
-          }
+          },
         }),
         'outline-blue': (props) => ({
           w: '100%',
@@ -227,10 +226,10 @@ const theme = extendTheme({
           },
           _active: {
             bgGradient: 'linear-gradient(to bottom right, #7F3BF2, #357DF8)',
-          }
+          },
         }),
       },
-    }
+    },
   },
 })
 
@@ -265,16 +264,20 @@ export const GitHubIcon = createIcon({
   displayName: 'GitHubIcon',
   viewBox: '0 0 24 24',
   path: (
-    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
   ),
 })
 
 export const TrashCanIcon = createIcon({
   displayName: 'TrashCanIcon',
   viewBox: '0 0 24 24',
-  fillRule: "evenodd",
-  clipRrule: "evenodd",
+  fillRule: 'evenodd',
+  clipRrule: 'evenodd',
   path: (
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2zm-7-10.414l3.293-3.293 1.414 1.414-3.293 3.293 3.293 3.293-1.414 1.414-3.293-3.293-3.293 3.293-1.414-1.414 3.293-3.293-3.293-3.293 1.414-1.414 3.293 3.293zm10-8.586h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-8-3h-4v1h4v-1z"/>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2zm-7-10.414l3.293-3.293 1.414 1.414-3.293 3.293 3.293 3.293-1.414 1.414-3.293-3.293-3.293 3.293-1.414-1.414 3.293-3.293-3.293-3.293 1.414-1.414 3.293 3.293zm10-8.586h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-8-3h-4v1h4v-1z"
+    />
   ),
 })
